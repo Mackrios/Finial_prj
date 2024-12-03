ee69ab40b05a77c856c80dbe0b40edbb832e0cf5
Macarios Atia <macarios.atia@gmail.com>
2024-12-02 22:43:23 -0500

    Fixed Isssues with code Signals now match the expected output

--- EGR365Final/EGR365Final.srcs/sources_1/imports/Downloads/Display.vhd
+++ EGR365Final/EGR365Final.srcs/sources_1/imports/Downloads/Display.vhd
@@ -21,7 +21,7 @@ end State_Machine;
 
 architecture Behavioral of State_Machine is
 
-    type state_type is (IDLE, STRT, READ_MSB, Read_LSB, DONE);
+    type state_type is (INIT, STRT, READ_MSB, Read_LSB, DONE);
     signal present_state, next_state : state_type;
     signal count_reset : std_logic;
     signal count : integer RANGE 0 to 1200;
@@ -35,7 +35,7 @@ begin
     clocked : PROCESS(CLK,RESET)
        BEGIN
          IF(RESET='1') THEN 
-           present_state <= IDLE;
+           present_state <= INIT;
         ELSIF(rising_edge(clk)) THEN
           present_state <= next_state;
         END IF;  
@@ -44,66 +44,52 @@ begin
     counter : PROCESS(clk)
        BEGIN
         IF(rising_edge(CLK)) THEN
-          IF(count_reset='1') THEN 
-            count <= 0;
-          ELSE
             count <= count + 1;
-          END IF;
-        END IF;  
+        END IF;
      END PROCESS counter;
 
     nextstate : PROCESS(present_state, count, START, DONE_O, ERR_O)
         BEGIN
             CASE present_state is
-                WHEN IDLE =>
-                     count_reset <= '0';
+                WHEN INIT =>
                      if( count = 1200 ) then
-                        count_reset <='1';
+                        count <= '0';
                         next_state <= STRT;
                      else
                        next_state <= present_state;
                      end if;
 
                 WHEN STRT =>
-                    count_reset <= '1';
                     if( START = '1') then
+                        count <= '0';
                         next_state <= READ_MSB;
                     else
                         next_state <= present_state;
                     end if;
 
                 WHEN READ_MSB =>
-                    count_reset <= '0';
-                    if ( count = 2) then 
-                        count_reset <= '1';
-                    else
-                        count_reset <= '0';
-                    end if;
                     if (DONE_O = '1') then
+                        count <= '0';
                         next_state <= Read_LSB;
                     elsif (ERR_O = '1') then
-                       next_state <= STRT;
+                        count <= '0' ;
+                        next_state <= STRT;
                     else
                         next_state <= present_state;
                     end if;
                     
                 WHEN Read_LSB =>
-                    count_reset <= '0';
-                    if (count = 510) then
-                        count_reset <= '1';
-                    else 
-                        count_reset <= '0';
-                    end if;
                     if (DONE_O = '1') then
+                        count <= '0';
                         next_state <= DONE;
                     elsif (ERR_O = '1') then
+                        count <= '0';
                         next_state <= STRT;
                     else 
                         next_state <= present_state;
                     end if;
 
                 WHEN DONE =>
-                    count_reset <= '1';
                     if (START = '0') then
                         next_state <= STRT;
                     else
@@ -115,7 +101,7 @@ begin
     output : PROCESS(present_state, START, count) 
            BEGIN
            CASE present_state is
-              WHEN IDLE =>
+              WHEN INIT =>
                     A_I <= addr & read_write_bit_0;
                     D_I <= (others=> '0');
                     if (count < 1) then
@@ -135,19 +121,9 @@ begin
 
               WHEN READ_MSB =>
                    A_I <= addr & read_write_bit_1;
-                   STB_I <= '1';
-                   if (count < 2) then
-                    MSG_I <= '1';
-                   else
-                    MSG_I <= '0';
-                   end if;
+                   MSG_I <= '0';
 
               WHEN Read_LSB =>
-                if (count = 1) then
-                    MSG_I <= '1';
-                else 
-                    MSG_I <= '0';
-                end if;
                 if (count > 509) then
                     STB_I <= '0';
                 else
@@ -156,7 +132,6 @@ begin
 
               WHEN DONE =>  
                 STB_I <= '0';
-                MSG_I <= '1';
           END CASE;
     END PROCESS output;
 
